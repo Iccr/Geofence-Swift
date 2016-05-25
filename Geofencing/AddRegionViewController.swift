d038bfa4913be1705c0a79e6ce23fe4794221e22
ccr <sis.ccr@gmail.com>
2016-05-25 10:57:54 +0545

    shaded region is passed from second to first controller. test annotation added when long pressed for testing.

--- Geofencing/AddRegionViewController.swift
+++ Geofencing/AddRegionViewController.swift
@@ -1,100 +1,135 @@
     //
-//  AddRegionViewController.swift
-//  Geofencing
-//
-//  Created by shishir sapkota on 5/23/16.
-//  Copyright © 2016 shishir sapkota. All rights reserved.
-//
-
-import UIKit
-import MapKit
+    //  AddRegionViewController.swift
+    //  Geofencing
+    //
+    //  Created by shishir sapkota on 5/23/16.
+    //  Copyright © 2016 shishir sapkota. All rights reserved.
+    //
+    
+    import UIKit
+    import MapKit
     
     
-    
+    // struct that holds the coortinate of all the annotations placed for the location
     struct PolyRegion {
         var title = String()
         var vertices = [CLLocationCoordinate2D]()
         mutating func addVertice(vertice: CLLocationCoordinate2D){
             self.vertices.append(vertice)
         }
+        
+        func Vertices()-> [CLLocationCoordinate2D] {
+            return self.vertices
+        }
     }
     
+    
+    //protocol to take all the location information to other viewController
     protocol RegionDelegate {
         func userdidAddRegion(region:PolyRegion)
     }
-
-class AddRegionViewController: UIViewController, RegionNameDelegate {
-    var pointArray: Array<CLLocationCoordinate2D> = []
     
-    @IBOutlet weak var mpView: MKMapView!
-   
-     
     
-    var delegate:RegionDelegate? = nil
-    
-    var polyRegion = PolyRegion()
-    var annotationTitle: String?
+    // main View controller
+    class AddRegionViewController: UIViewController, RegionNameDelegate, MKMapViewDelegate {
+        var pointArray: Array<CGPoint> = []
+        var locationManager = CLLocationManager()
+        @IBOutlet weak var mpView: MKMapView!
+        var allPoints: Array<CGPoint> = []
+        // protocols delegate
+        var delegate:RegionDelegate? = nil
+        
+        //holds all the coordinate for the location
+        var polyRegion = PolyRegion()
+        var annotationTitle: String?
         override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let rightBtn = UIBarButtonItem.init(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancel")
-        self.navigationItem.rightBarButtonItem = rightBtn
-        let saveBtn = UIBarButtonItem(title: "Save", style: .Plain, target: self, action: "save")
-        self.navigationItem.leftBarButtonItem = saveBtn
-    }
-    
-    
-
-    
-    @IBAction func btnAddPin(sender: UILongPressGestureRecognizer) {
-        if sender.state == .Began{
-        let location = sender.locationInView(mpView)
-        let locationCordinate = self.mpView.convertPoint(location, toCoordinateFromView: self.mpView)
-        polyRegion.addVertice(locationCordinate)
-        print("added location: \(locationCordinate)")
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = locationCordinate
-        self.mpView.addAnnotation(annotation)
+            super.viewDidLoad()
+            mpView.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            mpView.showsUserLocation = true
+            let rightBtn = UIBarButtonItem.init(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancel")
+            self.navigationItem.rightBarButtonItem = rightBtn
+            let saveBtn = UIBarButtonItem(title: "Save", style: .Plain, target: self, action: "save")
+            self.navigationItem.leftBarButtonItem = saveBtn
         }
-    }
-
-    
-    func save(){
         
- 
-        self.delegate?.userdidAddRegion(self.polyRegion)
-       self.navigationController?.popViewControllerAnimated(true)
         
-
-    }
-    
-    
-    func cancel() {
-        if let navController = self.navigationController {
-            navController.popViewControllerAnimated(true)
+        
+        
+        @IBAction func btnAddPin(sender: UILongPressGestureRecognizer) {
+            if sender.state == .Began{
+                let location = sender.locationInView(mpView)
+                
+                let locationCordinate = self.mpView.convertPoint(location, toCoordinateFromView: self.mpView)
+                polyRegion.addVertice(locationCordinate)
+                
+                
+                var vertices = polyRegion.Vertices()
+                print(vertices)
+                let line = MKPolygon(coordinates: &vertices, count: vertices.count)
+                
+                mpView.addOverlays([line], level: .AboveRoads)
+                
+                // create annotations and add
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = locationCordinate
+                self.mpView.addAnnotation(annotation)
+            }
         }
-
-    }
-    
-    func AddRegionTitle(title: String) {
-        polyRegion.title = title
-        annotationTitle = title
-
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        
+        
+        
+        func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+            
+            let polylineRenderer = MKPolygonRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 4
+            polylineRenderer.fillColor = UIColor.blueColor()
+            
+            
+            return polylineRenderer
+            
+        }
+        
+        func save(){
+            
+            
+            self.delegate?.userdidAddRegion(self.polyRegion)
+            self.navigationController?.popViewControllerAnimated(true)
+            
+            
+        }
+        
+        
+        func cancel() {
+            if let navController = self.navigationController {
+                navController.popViewControllerAnimated(true)
+            }
+            
+        }
+        
+        func AddRegionTitle(title: String) {
+            polyRegion.title = title
+            annotationTitle = title
+            
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            
+        }
+        
+        @IBAction func btnAddTitle(sender: AnyObject) {
+            let locationTitleViewController = self.storyboard?.instantiateViewControllerWithIdentifier("LocationTitleViewController") as! LocationTitleViewController
+            locationTitleViewController.delegate = self
+            
+            self.navigationController?.pushViewController(locationTitleViewController, animated: true)
+            
+        }
+        
+        
+        
         
     }
     
-    @IBAction func btnAddTitle(sender: AnyObject) {
-        let locationTitleViewController = self.storyboard?.instantiateViewControllerWithIdentifier("LocationTitleViewController") as! LocationTitleViewController
-        locationTitleViewController.delegate = self
-        
-        self.navigationController?.pushViewController(locationTitleViewController, animated: true)
-    
-    }
-    
-    
-   }
-
--- Geofencing/ViewController.swift
+++ Geofencing/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import MapKit
 import CoreLocation
-class ViewController: UIViewController, CLLocationManagerDelegate, RegionDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, RegionDelegate, MKMapViewDelegate {
 
    var locationManager = CLLocationManager()
-
     @IBOutlet weak var mpView: MKMapView!
     
    
@@ -23,6 +22,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, RegionDelegat
         locationManager.requestAlwaysAuthorization()
         locationManager.requestWhenInUseAuthorization()
         mpView.showsUserLocation = true
+        mpView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,18 +30,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate, RegionDelegat
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnTestPoint(sender: UILongPressGestureRecognizer) {
+        if sender.state == .Began{
+            self.mpView.removeAnnotations(self.mpView.annotations)
+            
+            let location = sender.locationInView(mpView)
+            
+            let locationCordinate = self.mpView.convertPoint(location, toCoordinateFromView: self.mpView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = locationCordinate
+            
+            self.mpView.addAnnotation(annotation)
+        }
+
+    }
     func userdidAddRegion(region: PolyRegion) {
-        
-        
+        if(region.vertices.count>2){
         let annotationLocations = region.vertices
         print(region.vertices.count)
-        for location in annotationLocations{
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = location
-            annotation.title = region.title
-            mpView.addAnnotation(annotation)
-        }
+//        for location in annotationLocations{
+//            let annotation = MKPointAnnotation()
+//            annotation.coordinate = location
+//            annotation.title = region.title
+//            mpView.addAnnotation(annotation)
+//            
+//            
+//        }
+        var vertices = annotationLocations
+        print(vertices)
+        let line = MKPolygon(coordinates: &vertices, count: vertices.count)
+        mpView.addOverlays([line], level: .AboveRoads)
+        }else{ print("no polygon detected")}
     }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        print("poly renderer")
+        let polylineRenderer = MKPolygonRenderer(overlay: overlay)
+        polylineRenderer.strokeColor = UIColor.blueColor()
+        polylineRenderer.lineWidth = 4
+        polylineRenderer.fillColor = UIColor.blueColor()
+        return polylineRenderer
+    }
+    
+
     
     @IBAction func addRegion(sender: AnyObject) {
         
